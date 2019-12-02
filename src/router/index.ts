8b795cb607ef373e488920363a28341658b4afef
Branko Bogdanovic <branko.bogd@gmail.com>
2019-12-02 23:19:50 +0100

    Added routing to login view.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
   {
     path: '/',
     name: 'home',
@@ -25,5 +31,6 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 })
+
 
 export default router
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,15 +5,10 @@
 </template>
 <script lang="ts">
   import Vue from 'vue';
-  import Component from 'vue-class-component';
 
-  @Component({})
   export default class Login extends Vue {
-
     mounted (){
-      console.log('hello from app');
     }
-    
   }
 
 </script>
